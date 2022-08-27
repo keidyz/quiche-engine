469246bd730311bdfa716f98459fa6c676895604
Keisha Zapra <kzapra@gmail.com>
2022-08-27 23:12:39 +0800

    Add tsconfig, MainMenu sample component, and declaration for media files

---
+++ src/components/main-menu/main-menu.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import styled from 'styled-components'
+import background from '#game-development-files/main-menu/bg.png';
+
+const Background = styled.img`
+    width: 100%;
+`;
+
+export const MainMenu = () => {
+    return (
+        <Background src={background} />
+    );
+};
